da8377aca45d5aadd79cfff4f15f50b7e09da12c
saumya <saumya20personal@gmail.com>
2026-05-16 00:15:03 +0530

    error fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  css: {
+    lightningcss: {
+      errorRecovery: true,
+    },
+  },
 
   server: {
     port: 5173,
@@ -13,4 +19,4 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: false,
   },
-})
+});
